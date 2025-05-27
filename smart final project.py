edc4a5cf260499fd209338ac4be1e3dd47185427
Adham Elbadry <adhamelbadrykteer@gmail.com>
2025-05-27 07:09:16 +0300

    Update smart final project.py
    changed k to best value

--- smart final project.py
+++ smart final project.py
@@ -54,7 +54,7 @@ y_encoded = le.fit_transform(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
 
 # 🤖 STEP 6: Train KNN
-knn = KNeighborsClassifier(n_neighbors=3)
+knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(X_train, y_train)
 
 # 📊 STEP 7: Predict and evaluate
